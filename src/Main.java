d1b131f51fcafed65d7b56d72311b029c2888132
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2022-12-02 15:51:33 +0300

    Some fixes

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 
 import java.util.LinkedList;
-
 public class Main {
 
     public static void main(String[] args) {
 
-//burda txtleri cekicem
+//burda txtleri cekerim
         char[] charArray = {'a', 'b', 'c', 'd', 'e', 'f'};
         int[] charfreq = {5, 9, 12, 13, 16, 45};
         int n = 6;
-
-        LinkedList<HNode> q = new LinkedList<HNode>();
+        LinkedList<HNode> llist = new LinkedList<HNode>();
 
         for (int i = 0; i < n; i++) {
 
@@ -22,46 +20,33 @@ public class Main {
             node.left = null;
             node.right = null;
 
-            // add functions adds
-            // the huffman node to the queue.
-            q.add(node);
+            llist.add(node);
         }
-        System.out.println(q);
+
 
 
         HNode root = null;
-        while (q.size() > 1) {
+        while (llist.size() > 1) {
 
-            // first min extract.
-            HNode x = q.peek();
-            q.poll();
+            HNode x = llist.peek();
+            llist.poll(); //nodeye eşitledikten sonra sildik
 
-            // second min extract.
-            HNode y = q.peek();
-            q.poll();
+            HNode y = llist.peek();
+            llist.poll();
 
-            // new node f which is equal
             HNode f = new HNode();
 
-            // to the sum of the frequency of the two nodes
-            // assigning values to the f node.
-            f.data = x.data + y.data;
+            f.data = x.data + y.data; // dataların toplamına eşitledik
             f.ch = '-';
 
-            // first extracted node as left child.
-            f.left = x;
 
-            // second extracted node as the right child.
+            f.left = x;
             f.right = y;
 
-            // marking the f node as the root node.
             root = f;
-
-            // add this node to the priority-queue.
-            q.add(f);
+            llist.add(f);
         }
 
-        // print the codes by traversing the tree
         Huffman.printCode(root, "");
     }
 }
